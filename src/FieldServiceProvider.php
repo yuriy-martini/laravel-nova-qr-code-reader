52b6b10a90d8c35b0a43447d4d40478cd5349b1c
Yuriy Martini <yuriymartini@gmail.com>
2022-03-04 17:22:20 +0100

    refactoring

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -4,7 +4,6 @@ namespace Tsungsoft\QrCodeReader;
 
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
-use Laravel\Nova\Events\ServingNova;
 use Laravel\Nova\Nova;
 
 class FieldServiceProvider extends ServiceProvider
@@ -18,29 +17,20 @@ class FieldServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../resources/lang' => App::resourcePath('lang/vendor/qr-code-reader'),
+                __DIR__ . '/../resources/lang' => App::resourcePath('lang/vendor/qr-code-reader'),
             ], ['qr-code-reader', 'lang', 'qr-code-reader-lang']);
         }
 
-        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'qr-code-reader');
+        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'qr-code-reader');
 
-        Nova::serving(function (ServingNova $event) {
-            Nova::script('qr-code-reader', __DIR__.'/../dist/js/field.js');
-            Nova::style('qr-code-reader', __DIR__.'/../dist/css/field.css');
+        Nova::serving(function () {
+            Nova::script('qr-code-reader', __DIR__ . '/../dist/js/field.js');
+            Nova::style('qr-code-reader', __DIR__ . '/../dist/css/field.css');
 
+            $locale = App::getLocale();
             Nova::translations(
-                App::resourcePath('lang/vendor/qr-code-reader/'.app()->getLocale().'.json')
+                App::resourcePath("lang/vendor/qr-code-reader/$locale.json")
             );
         });
-    }
-
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
     }
 }
